c0bbc67cf208daa7129ba6a5a9ee81bbda00aadb
ismaelyoussef1009 <ismaelyoussef1009>
2022-12-30 16:56:40 +0100

    working on food section desing

---
+++ javascript/inde.js
@@ -0,0 +1,10 @@
+var names = document.querySelectorAll(".sub_menu_section");
+
+
+names.forEach(para =>{
+    para.addEventListener('click', function(){
+        para.classList.add('active');
+        para.style.background = 'red'
+
+    })
+})
