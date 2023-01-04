8ac5acb338443d95e62bf6a41ca1cad833628836
ismaelyoussef1009 <ismaelyoussef1009>
2023-01-04 16:58:46 +0100

    adding mixin and variable in my projec

--- javascript/inde.js
+++ javascript/inde.js
@@ -4,7 +4,7 @@ var names = document.querySelectorAll(".sub_menu_section");
 names.forEach(para =>{
     para.addEventListener('click', function(){
         para.classList.add('active');
-        para.style.background = 'red'
+        para.style.background = 'red';
 
     })
 })
