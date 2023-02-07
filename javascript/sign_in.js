74632896f3699a573ae098b1cbdeb0a98cff3133
ismaelyoussef1009 <ismaelyoussef1009>
2023-02-07 12:25:20 +0100

    Almust done

--- javascript/sign_in.js
+++ javascript/sign_in.js
@@ -2,6 +2,7 @@ let usernames = document.querySelector("input[type = text]");
 let Email = document.querySelector("input[type = email]");
 let passwords = document.querySelector("input[type = password]");
 let setbtn = document.querySelector(".btn");
+let h1 = document.getElementById("man");
 
 let btn = document.getElementById('you');
 let form = document.getElementById('isma');
@@ -36,12 +37,17 @@ let myUsers = "users";
 setbtn.addEventListener("click", youssef =>{
     youssef.preventDefault();
     const btnUser = localStorage.getItem(myUsers);
+    // const h1 = localStorage.getItem(usernames.value);
     const User = JSON.parse(btnUser);
 
     for(i = 0; i < User.length; i++){
         if (usernames.value == User[i].username){
             if(passwords.value == atob(User[i].password)){
-                alert("welcome to you");
+                alert(`${usernames.value} welcome to you`);
+                // h1.style.innerhtml = `${usernames.value}`;
+                h1.innerHTML = `${usernames.value} welcome!`;
+                console.log(usernames.value);
+                // `${usernames.value} Account created successfully`;
             }
             else {
                 alert('password not found');
--- javascript/sign_up.js
+++ javascript/sign_up.js
@@ -2,6 +2,7 @@ let usernames = document.querySelector("input[type = text]");
 let Email = document.querySelector("input[type = email]");
 let passwords = document.querySelector("input[type = password]");
 let createAccount = document.querySelector(".btn2");
+let h1 = document.getElementById("man");
 
 
 
@@ -15,13 +16,42 @@ createAccount.addEventListener("click", ismael =>{
         email: Email.value,
         password: btoa(passwords.value),
     }
+    
+    // checking for user
+    if (usernames.value == ''  || usernames.value == null){
+        alert('user name is required');
+        return;
+    }
 
-    users.push(user);
-    localStorage.setItem(myUsers, JSON.stringify(users));
 
-    // localStorage.setItem(usernames.value,JSON.stringify(user));
-    // localStorage.setItem(password.value,password.value);
+    for( i = 0; i < users.length; i++){
+        if (usernames.value == users[i].username){
+            alert(`${usernames.value} Already exssite`);
+            return;
+        
+        }
+    };
+
+    // checking for email
+    for( i = 0; i < users.length; i++){
+        if (Email.value ==  users[i].email){
+            alert('email already exist');
+            return;
+        
+        }
+    };
+
+    if(users.push(user)){
+        localStorage.setItem(myUsers, JSON.stringify(users));
+        // `${usernames.value} Account created successfully`
+        alert(`${usernames.value} Account created successfully`);
+        location.href = 'index.html';
+    }
+    
     const btnUser = localStorage.getItem(myUsers);
+    const h1 = localStorage.getItem(usernames.value)
     const User = JSON.parse(btnUser);
-    console.log(User);
+    // console.log(User);
+
+   
 });
